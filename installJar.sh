c951ce8ca913a6a93be17df33bcea2d1ed2443a7
Bogdan Drutu <bogdandrutu@gmail.com>
2018-09-26 19:23:12 -0700

    Remove maven support.

--- installJar.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mvn install && mvn install:install-file -Dfile=$(pwd)/target/opencensus-jdbc-0.0.2.jar \
-    -DgroupId=io.opencensus.integration -DartifactId=opencensus-jdbc -Dversion=0.0.2 \
-    -Dpackaging=jar -DgeneratePom=true
